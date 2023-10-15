1b8c4d36666525925c3dd624030856af4b0c9118
riyanada <riyanmaulana402@yahoo.co.id>
2023-10-15 15:08:30 +0700

    add tugas-3

---
+++ tugas-3/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('judul_halaman', 'Halaman Home')
+@section('konten')
+    <p>Ini Adalah Halaman Home</p>
+@endsection
---
+++ tugas-3/resources/views/tentang.blade.php
@@ -0,0 +1,25 @@
+@extends('master')
+
+@section('judul_halaman', 'Halaman Tentang')
+@section('konten')
+    <p>Ini Adalah Halaman Tentang</p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum erat eget facilisis lacinia. Nullam lacinia
+        nisi a enim aliquam porta. Mauris consequat ac nulla sit amet accumsan. Sed dapibus tristique maximus. Aliquam
+        vestibulum dui sit amet velit vulputate accumsan. Pellentesque ac tincidunt dolor. Proin a facilisis sapien, a
+        consequat nulla. Morbi tempor velit non nibh euismod, a maximus turpis facilisis. Nulla facilisi. Curabitur
+        scelerisque et urna ac sollicitudin.
+
+        Ut lectus mauris, mollis vitae placerat a, placerat non neque. Suspendisse nisl arcu, efficitur vel accumsan eget,
+        elementum id tellus. Integer id magna eu leo interdum congue vel at tortor. Duis mattis, tortor vestibulum ultrices
+        suscipit, tortor nibh rutrum ex, quis faucibus ex urna venenatis ligula. Nulla facilisi. Donec luctus sem dapibus,
+        maximus enim sed, fringilla risus. Sed vel quam vel odio pharetra ornare. Nullam placerat libero sed justo viverra
+        consequat. Nunc nec velit viverra, malesuada neque auctor, interdum eros. Praesent sed volutpat augue.
+
+        Nunc ullamcorper semper justo, ut volutpat neque venenatis et. Aenean luctus nulla vitae varius condimentum. Nulla
+        vehicula posuere turpis. Morbi bibendum a ligula eget mattis. Fusce eget urna sed purus tempor sollicitudin. In
+        tortor nulla, facilisis ornare vestibulum sed, auctor vel lectus. Integer tempus ultrices eros, tempus scelerisque
+        lorem faucibus sed. Aenean felis ex, sagittis vitae ligula eu, consequat varius enim. Cras eget dolor id quam
+        viverra condimentum quis a purus. Suspendisse nisl lectus, maximus et ligula maximus, semper fringilla ligula.
+    </p>
+@endsection
