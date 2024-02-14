5e870d2e860eb14245fa44f32052ba9c5b4a6a5d
riyanada <riyanmaulana402@yahoo.co.id>
2024-02-15 04:20:47 +0700

    updated tugas-6

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+// use Auth;
+
+class ProductController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $this->validate(request(), [
+            'name' => 'required|unique:products,name',
+            'price' => 'required|numeric',
+            'description' => 'required',
+        ]);
+
+
+        $product = new Product();
+        $product->user_id = Auth::user()->id;
+        $product->name = $request->post('name');
+        $product->price = $request->post('price');
+        $product->description = $request->post('description');
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            $path = $image->storeAs(env('IMAGE_UPLOAD_PATH'), $filename, 'public'); // Menyimpan gambar ke penyimpanan publik
+            $product->image_url = asset(Storage::url($path)); // Membuat URL lengkap dan menyimpannya ke dalam atribut image_url
+        }
+
+
+
+        $product->save();
+
+        return redirect('admin/products')->with('succes', 'Produk Berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('admin.products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('admin.products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+
+        return redirect('admin/products')->with('success', 'Produk berhasil diperbarui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('admin.products.index')->with('success', 'Produk berhasil dihapus.');
+    }
+}
